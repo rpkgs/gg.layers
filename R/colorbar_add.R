f54818c314bdaa77bb125d645943e7a5f57eaa00
Dongdong Kong <kongdd@users.noreply.github.com>
2022-03-01 15:52:14 +0800

    improve `add_colorbar`

--- R/colorbar_add.R
+++ R/colorbar_add.R
@@ -1,17 +1,35 @@
+#' add_colorbar
+#' 
+#' @param width 
+#' 
 #' @import gtable ggplotify
 #' @importFrom ggplot2 ggplotGrob ggplot_add
 #' @export
-add_colorbar <- function(p, g, width = 1) {
+add_colorbar <- function(p, g, width = NULL, 
+    title = NULL, 
+    legend.title = element_text(hjust = 0, vjust = 0, size = 14, family = "Times")) 
+{
     if (!("gtable" %in% class(p))) p <- ggplotGrob(p)
     dim = dim(p)
 
     loc = p$layout %>% subset(grepl("panel", name)) %>% .[nrow(.), ]
 
-    width = grid::grobWidth(g) * width
+    if (!is.null(title)) {
+        g_title = element_grob(legend.title, title, x = 0, y = 0.5)
+    } else {
+        g_title = nullGrob()
+    }
+
+    if (is.null(width)) {   
+        width = max(grobWidth(g), grobWidth(g_title))
+    }
+    
+    # width = grid::grobWidth(g) * width
     p2 = p %>% gtable_add_cols(width)
     # p2$layout$clip <- "on"
     # g = as.grob(g)
     ans = gtable_add_grob(p2, g, l = dim[2] + 1, t = loc$t, b = loc$b, clip = "off")
+    ans <- gtable_add_grob(ans, g_title, l = dim[2] + 1, t = loc$t - 1, clip = "off")
     ans
     # as.ggplot(ans)
 }
