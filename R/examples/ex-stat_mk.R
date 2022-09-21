2c57bd0aa52c8ceaa95f9a2a79aa1b637c574f73
Dongdong Kong <kongdd@users.noreply.github.com>
2022-09-21 13:05:44 +0800

    improve `stat_mk`

--- R/examples/ex-stat_mk.R
+++ R/examples/ex-stat_mk.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-ggplot(mpg, aes(displ, hwy, colour = drv)) + 
-    geom_point() + 
-    stat_mk(size = 1) + 
+ggplot(mpg, aes(displ, hwy, colour = drv)) +
+    geom_point() +
+    stat_mk(size = 1, slope_FUN = slope_mk) +
     geom_smooth(method = "lm", se = FALSE, linetype = 2)
--- R/stat_mk.R
+++ R/stat_mk.R
@@ -1,14 +1,20 @@
 #' @import ggplot2
 StatMK <- ggproto("StatMK", Stat,
   required_aes = c("x", "y"),
-  compute_group = function(data, scales, ...) {
-    rng <- range(data$x, na.rm = TRUE)
-    grid <- data.frame(x = rng)
-    mod <- mkTrend(data$y, data$x)
-    grid$y <- grid$x * mod["slp"] + mod["intercept"]
+  compute_group = function(data, scales, slope_FUN = slope_mk) {
+    x <- data$x
+    y <- data$y
+    slp = slope_FUN(y, x)["slope"]
+    # rng <- range(data$x, na.rm = TRUE)
+    # grid <- data.frame(x = rng)
+    # mod <- mkTrend(data$y, data$x)
+    # grid$y <- grid$x * mod["slp"] + mod["intercept"]
     # mod <- lm(y ~ x, data = data)
     # grid$y <- predict(mod, newdata = grid)
-    grid
+    # grid
+    intercept <- mean(y - slp * x, na.rm = T)
+    df = data.frame(intercept = intercept, slope = slp)
+    df
   }
 )
 
@@ -20,15 +26,17 @@ StatMK <- ggproto("StatMK", Stat,
 #' just like [ggplot2::stat_smooth()].
 #'
 #' @example R/examples/ex-stat_mk.R
-#'
+#' 
 #' @import rtrend
 #' @export
-stat_mk <- function(mapping = NULL, data = NULL, geom = "line",
-                    position = "identity", na.rm = FALSE, show.legend = NA,
+stat_mk <- function(mapping = NULL, data = NULL, geom = "abline",
+                    position = "identity",
+                    slope_FUN = slope_mk,
+                    na.rm = FALSE, show.legend = NA,
                     inherit.aes = TRUE, ...) {
   layer(
     stat = StatMK, data = data, mapping = mapping, geom = geom,
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-    params = list(na.rm = na.rm, ...)
+    params = list(slope_FUN = slope_FUN, na.rm = na.rm, ...)
   )
 }
